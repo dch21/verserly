b4577020675f44656672e116ad4c7a845c548215
dchu <danchu56@gmail.com>
2020-10-12 16:05:41 -0400

    Erasure Fully Working

--- src/index.js
+++ src/index.js
@@ -40,6 +40,23 @@ window.changeSelectionOption = function changeSelectionOption(option) {
 
 
 
+// window.submitText = function submitText() {
+//   const submittedText = document.getElementById("submitedText").value;
+
+//   submittedText.split("").forEach((letter) => {
+
+//     const span = document.createElement("span");
+//     span.className = "visible";
+//     span.innerHTML = letter;
+//     span.addEventListener("click", function() {
+//       this.classList.toggle(`${window.erasureOption}`);
+//     });
+
+//     const paragraph = document.getElementById("targetText");
+//     paragraph.append(span);
+//   });
+// };
+
 window.submitText = function submitText() {
   const submittedText = document.getElementById("submitedText").value;
 
@@ -49,13 +66,42 @@ window.submitText = function submitText() {
     span.className = "visible";
     span.innerHTML = letter;
     span.addEventListener("click", function() {
-      this.classList.toggle(`${window.erasureOption}`);
+      if (window.erasureSelection === "character") {
+        this.classList.toggle(`${window.erasureOption}`);
+      } else {
+        getWord(this).forEach((letterElement)=>{
+          letterElement.classList.toggle(`${window.erasureOption}`);
+        })
+      }
     });
 
     const paragraph = document.getElementById("targetText");
     paragraph.append(span);
   });
 };
+
+function getWord(ele) {
+  let prev = ele.previousElementSibling
+  let next = ele.nextElementSibling
+  let letters = [ele]
+  while (prev !== null && prev.innerHTML !== " ") {
+    letters.push(prev);
+    prev = prev.previousElementSibling;
+    if (prev.innerHTML === " ") {
+      break;
+    }
+  }
+  
+  while (next !==  null && next.innerHTML !== " ") {
+      letters.push(next)
+      next = next.nextElementSibling;
+      if (next.innerHTML === " ") {
+        break;
+      }
+  }
+  
+  return letters
+}
 
 window.clearText = function clearText() {
   const submittedText = document.getElementById("targetText").innerHTML = "";
